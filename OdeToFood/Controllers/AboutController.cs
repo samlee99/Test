9fd02f721bd39cc32bb1f5789039b8ba7a296404
samlee99 <sam1994lee@gmail.com>
2018-04-06 17:41:40 -0700

    First commit

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")] // using the controller token needs to be in square brackets, and it tells the MVC framework the name of the controller from the class name.
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "1+555+555+5555";
+        }
+        
+        public string Address()
+        {
+            return "USA";
+        }
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new Restaurant { Id = 1, Name = "Scott's Pizza Place" };
+            return View();
+        }
+    }
+}
